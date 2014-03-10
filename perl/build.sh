fee91ef49fe6a6870d78d04346bff664fea5a3d2
Dan Blanchard <dblanchard@ets.org>
2014-03-10 16:42:01 -0400

    Update Perl recipe again based on conda-recipes version.

--- perl/build.sh
+++ perl/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Build perl
-sh Configure -de -Dprefix=$PREFIX -Duserelocatableinc
+sh Configure -de -Dprefix=$PREFIX -Dsiteprefix=$PREFIX/cpanm -Duserelocatableinc
 make
 # make test
 make install
 
 # Install CPAN Minus to make building other packages that rely on this simpler
 echo "Installing CPAN Minus"
-curl -L http://cpanmin.us | perl - App::cpanminus
+curl -L http://cpanmin.us | perl - --local-lib-contained $PREFIX/cpanm App::cpanminus
