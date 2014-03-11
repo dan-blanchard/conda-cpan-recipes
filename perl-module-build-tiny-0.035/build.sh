e04521ec8ebed898192564cb290436646ee98668
Dan Blanchard <dblanchard@ets.org>
2014-03-11 11:58:43 -0400

    Switch Build.PL/Makefile.PL precedence in Module::Build::Tiny recipe.

--- perl-module-build-tiny-0.035/build.sh
+++ perl-module-build-tiny-0.035/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# If it has Makefile.PL use that, otherwise use Build.PL
-if [[ -e Makefile.PL ]]; then
-    # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
-    make
-    make test
-    make install
-elif [[ -e Build.PL ]]; then
+# If it has Build.PL use that, otherwise use Makefile.PL
+if [[ -e Build.PL ]]; then
     perl Build.PL
     ./Build
     ./Build test
     # Make sure this goes in site
     ./Build install --installdirs site
+elif [[ -e Makefile.PL ]]; then
+    # Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
+    make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
