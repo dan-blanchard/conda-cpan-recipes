25ff76014637d17f53e832a2e9ebc8cf63b7c41b
Daniel Blanchard <dblanchard@ets.org>
2014-06-06 09:16:42 -0400

    Update ZMQ::FFI to 0.12

---
+++ perl-zmq-ffi-0.12/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Hard-code environment prefix in Util.pm so that it gets replaced later.
+sed -i -e "s,CONDA_PREFIX_PLACEHOLDER,$PREFIX/lib," ./lib/ZMQ/FFI/Util.pm
+
+# If it has Makefile.PL use that, otherwise use Build.PL
+if [[ -e Makefile.PL ]]; then
+    # Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
+    make install
+elif [[ -e Build.PL ]]; then
+    perl Build.PL
+    ./Build
+    ./Build test
+    # Make sure this goes in site
+    ./Build install --installdirs site
+else
+    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
+    exit 1
+fi
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
